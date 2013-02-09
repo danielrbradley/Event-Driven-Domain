f4a12e5ec5c96efdcd8f341ed53af6e2e9036467
Daniel Bradley <daniel@danielbradley.net>
2013-02-09 12:56:01 +0000

    Create simple pass through validating writer

---
+++ EventDrivenDomain.Tests/Fakes/PassThroughEventStreamValidatingWriter.cs
@@ -0,0 +1,16 @@
+﻿namespace EventDrivenDomain.Tests.Fakes
+{
+    using System.IO;
+
+    /// <summary>
+    /// A test validating writer which skips any kind of hash validation and simply
+    /// writes the inner stream to the outer stream.
+    /// </summary>
+    public class PassThroughEventStreamValidatingWriter : IEventStreamValidatingWriter
+    {
+        public void Write(Stream stream, string previousHash, Stream innerStream)
+        {
+            innerStream.CopyTo(stream);
+        }
+    }
+}
