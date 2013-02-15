aad32c84e582dd5509c28e36f15cf65458417564
Daniel Bradley <daniel@danielbradley.net>
2013-02-15 08:12:40 +0000

    Create simple DateTime UtcNow timestamp provider

---
+++ EventDrivenDomain.EventStore/SystemClockUtcTimestampProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace EventDrivenDomain.EventStore
+{
+    using System;
+
+    public class SystemClockUtcTimestampProvider : ITimestampProvider
+    {
+        public DateTime GetTimestamp()
+        {
+            return DateTime.UtcNow;
+        }
+    }
+}
