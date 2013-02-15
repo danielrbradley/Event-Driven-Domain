b9f56b348d028561ff1a00c92f5c593ce99eb5b8
Daniel Bradley <daniel@danielbradley.net>
2013-02-15 08:24:33 +0000

    Implement a directory event store write lock
    Create the write lock acquisition exception type.

---
+++ EventDrivenDomain.EventStore.LocalFileSystem/DirectoryEventStoreWriteLock.cs
@@ -0,0 +1,69 @@
+﻿namespace EventDrivenDomain.EventStore.LocalFileSystem
+{
+    using System;
+    using System.IO;
+    using System.Threading;
+
+    public class DirectoryEventStoreWriteLock : IEventStoreWriteLock
+    {
+        private readonly string lockFilePath;
+
+        private readonly int millisecondlockWaitSleep;
+
+        public const string DefaultLockFilename = "eventstore.lock";
+
+        public const int DefaultMillisecondLockWaitSleep = 10000;
+
+        public DirectoryEventStoreWriteLock(
+            string path,
+            string lockFilename = DefaultLockFilename,
+            int millisecondlockWaitSleep = DefaultMillisecondLockWaitSleep)
+        {
+            this.millisecondlockWaitSleep = millisecondlockWaitSleep;
+            this.lockFilePath = Path.Combine(path, lockFilename);
+        }
+
+        public IDisposable WaitAquire()
+        {
+            var aquired = false;
+            while (!aquired)
+            {
+                if (!File.Exists(lockFilePath))
+                {
+                    try
+                    {
+                        using (var file = File.Open(this.lockFilePath, FileMode.CreateNew))
+                        {
+                        }
+
+                        aquired = true;
+                        var writeLock = new DirectoryWriteLock(this.lockFilePath);
+                        return writeLock;
+                    }
+                    catch (IOException)
+                    {
+                    }
+                }
+
+                Thread.Sleep(this.millisecondlockWaitSleep);
+            }
+
+            throw new WriteLockAcquisitionException("Failed to aquire directory write lock");
+        }
+
+        internal class DirectoryWriteLock : IDisposable
+        {
+            private readonly string lockFilePath;
+
+            public DirectoryWriteLock(string lockFilePath)
+            {
+                this.lockFilePath = lockFilePath;
+            }
+
+            public void Dispose()
+            {
+                File.Delete(this.lockFilePath);
+            }
+        }
+    }
+}
---
+++ EventDrivenDomain.EventStore/WriteLockAcquisitionException.cs
@@ -0,0 +1,29 @@
+﻿namespace EventDrivenDomain.EventStore
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    public class WriteLockAcquisitionException : Exception
+    {
+        public WriteLockAcquisitionException()
+        {
+        }
+
+        public WriteLockAcquisitionException(string message)
+            : base(message)
+        {
+        }
+
+        public WriteLockAcquisitionException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        protected WriteLockAcquisitionException(
+            SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
